06cc79a4527c7d3b5a3277c4d56039a297bf02ee
kyohei kabasawa <s.life.kb@icloud.com>
2022-03-09 06:42:50 +0900

    2022/3/9

--- javascript_7.js
+++ javascript_7.js
@@ -3,4 +3,4 @@ function highAndLow(numbers){
   return Math.max.apply(null, numbers) + ' ' + Math.min.apply(null, numbers);
 }
 
-console.log(highAndLow("8 3 -5 42 -1 0 0 -9 4 7 4 -4")
+console.log(highAndLow("8 3 -5 42 -1 0 0 -9 4 7 4 -4"));

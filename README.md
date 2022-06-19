865352c96b34356d45b9ff16d176115c7c5c27e5
kyohei kabasawa <slifekb2@gmail.com>
2022-06-19 15:03:58 +0900

    typescript環境整備

--- README.md
+++ README.md
@@ -3,3 +3,7 @@ node (file name)
 
 # ruby execution
 ruby (file name)
+
+# TypeScript execution
+tsc (file name.ts)
+node (file name.js)
---
+++ TypeScript/typeScript_1.js
@@ -0,0 +1,8 @@
+var User = /** @class */ (function () {
+    function User(name) {
+        this.name = name;
+    }
+    return User;
+}());
+var u = new User("taro");
+console.log(u.name);
